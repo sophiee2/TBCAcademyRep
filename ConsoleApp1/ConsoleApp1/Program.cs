389681c6db143e4a23ccfd395623c000ecfc1a89
sophiee2 <sophie.gogashvili@gmail.com>
2023-06-05 18:08:10 +0400

    1  task

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string _ = Console.ReadLine();
+
+int a;
+Int32.TryParse(_, out a);
+if (a < 0)
+{
+    Console.WriteLine("cold");
+}else if (a >=0 && a<=30)
+{
+    Console.WriteLine("good weather");
+}else
+{
+    Console.WriteLine("Hot");
+}
