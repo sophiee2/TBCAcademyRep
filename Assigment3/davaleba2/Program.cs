af5f6efa58418de4c095b8d1141849c9eea25417
sophiee2 <sophie.gogashvili@gmail.com>
2023-06-07 17:59:43 +0400

    task 3

---
+++ Assigment3/davaleba2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("please enter numbers as many as you want, it will sum only positive numbers");
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0, avrg = 0, avrgcounter = 0;
+
+int[] arr =  new int[number];
+
+for (int i = 0; i < number; i++)
+{
+    if (arr[i] > 0)
+    {
+        sum+= arr[i];
+        avrgcounter++;
+    }
+}
+if (avrgcounter > 0)  avrg = sum / avrgcounter;
+
+Console.WriteLine("sum of positive numbers are " +  sum);
+Console.WriteLine("Average of positive numbers are " + avrg);
+
+Console.ReadKey();
+
+
