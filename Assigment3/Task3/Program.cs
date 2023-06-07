8216a13c04d1de0406249ef6265241309121b08c
sophiee2 <sophie.gogashvili@gmail.com>
2023-06-07 17:43:10 +0400

    task 4

---
+++ Assigment3/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("please Enter a number for pyramid: ");
+string input = Console.ReadLine();
+int rowNumber;
+int count = 0;
+Int32.TryParse(input, out rowNumber);
+
+
+
+for (int i = 1; i <= rowNumber; i++)
+{
+
+    for (int j = 1; j <= rowNumber - i; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int k = 1; k <= 2 * i - 1; k++)
+    {
+        Console.Write("*");
+    }
+
+
+    Console.WriteLine();
+}
+
+
+Console.ReadKey();
+
