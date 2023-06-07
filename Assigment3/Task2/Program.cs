7894b1f598926faf116d48d1f89d1724c277cd06
sophiee2 <sophie.gogashvili@gmail.com>
2023-06-07 17:22:08 +0400

    task 2

---
+++ Assigment3/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("please Enter a number: ");
+string input = Console.ReadLine();
+int number;
+int count = 0;
+Int32.TryParse(input, out number);
+
+for (int i = 1; i <= number; i++)
+{
+    if (number % i == 0) count ++;
+}
+Console.WriteLine(number + "- number's divider's count are " + count);
+
+Console.ReadKey();
