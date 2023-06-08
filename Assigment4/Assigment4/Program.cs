6b46764f7d7c45869999ed0b509bd63e9e2ba250
sophiee2 <sophie.gogashvili@gmail.com>
2023-06-08 10:46:30 +0400

    assigment4

---
+++ Assigment4/Assigment4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("How many numbers do you want to have in massive?");
+int count,number;
+Int32.TryParse(Console.ReadLine(), out count);
+int [] myarr = new int[count];
+for (int i = 0; i < myarr.Length; i++)
+{    number=Int32.Parse(Console.ReadLine());
+    myarr[i] = number;
+    i++;
+    Console.WriteLine(myarr[i])
+}
+
+Console.ReadKey();
+
