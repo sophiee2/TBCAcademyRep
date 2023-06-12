52204e6efefe89400515ed351f2f30d735c0d6f8
sophiee2 <sophie.gogashvili@gmail.com>
2023-06-12 11:49:46 +0400

    full task 2

--- Assigment5/Task2/Program.cs
+++ Assigment5/Task2/Program.cs
@@ -1,22 +1,69 @@
 ﻿using System;
-
+using System.Data;
 
 Console.WriteLine("please fill array length");
 Int32.TryParse(Console.ReadLine(), out int length);
 
 int arrayLength = length;
 
-Char[] myArr = new Char[length];
+char[] myArr = new char[length];
 
-void fillCharMassive(char[] array)
+char[] CharArray = FillCharMassive(myArr, length);
+
+//print char massive
+foreach (char c in CharArray)
 {
-    for (int i = 0; i < arrayLength; i++)
+    Console.WriteLine(c);
+}
+
+
+
+
+
+
+
+Console.Write("Enter a symbol: ");
+char symbol = Console.ReadKey().KeyChar;
+Console.WriteLine();
+
+int count = CheckCharNumbersInMassive(CharArray, symbol);
+
+PrintSymbolCount(symbol, count);
+
+void PrintSymbolCount(char symbol, int count)
+{
+    Console.WriteLine($"Symbol '{symbol}' occurs {count} times.");
+}
+
+
+//fillCharMassive
+char[] FillCharMassive(char[] charArray, int length)
+{
+    for (int i = 0; i < length; i++)
     {
-        array[i] = Char.Parse(Console.ReadLine());
+        char.TryParse(s: Console.ReadLine(),out char s);
+        charArray[i] = s;
     }
 
-    foreach (char c in array) {
-        {
-            Console.WriteLine(c);
-        }
+        return charArray;
+
+ 
 }
+
+
+int CheckCharNumbersInMassive(char[] NewCharArray, char symbol)
+{
+  
+        int count = 0;
+
+        for (int i = 0; i < NewCharArray.Length; i++)
+        {
+            if (NewCharArray[i] == symbol)
+                count++;
+        }
+
+        return count;
+ 
+}
+
+Console.ReadKey();
