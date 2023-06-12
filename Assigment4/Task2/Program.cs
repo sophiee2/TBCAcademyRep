2ba796b2ec3617705fbc1ef4785461231d21ef03
sophiee2 <sophie.gogashvili@gmail.com>
2023-06-12 10:48:08 +0400

    task1

---
+++ Assigment4/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("How many numbers do you want to have in massive?");
+int N, number;
+Int32.TryParse(Console.ReadLine(), out N);
+string[] myarr = new String[N];
+int[] numberArr =  new int[N];
+for (int i = 0; i < N; i++)
+
+{
+    Console.WriteLine("enter string");
+    myarr[i] = Console.ReadLine();
+    numberArr[i] = i;
+}
+
---
+++ Assigment5/Assigment5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+Console.WriteLine("please enter index");
+Int32.TryParse(Console.ReadLine(), out int a);
+int index = a;
+Console.WriteLine("please fill array length");
+Int32.TryParse(Console.ReadLine(), out int length);
+
+int arrayLength= length;
+
+int[] myArr = new int[length];
+
+
+
+int sum = PrintSumOfIndexedNumber(myArr, index);
+Console.WriteLine($"Sum of digits at index {index}: {sum}");
+
+int PrintSumOfIndexedNumber(int[] numberArr, int index)
+{
+
+    for (int i = 0; i <length; i++)
+    {
+        Int32.TryParse(Console.ReadLine(), out int n);
+        numberArr[i] = n;
+        if (i == index)
+        {
+            int element=n;
+            int sum = 0;
+
+            while (element > 0)
+            {
+
+                sum += element % 10;
+                element /= 10;
+
+
+            }
+            return sum;
+
+        }
+           
+ 
+        
+        
+    }
+    return 0;
+}
+Console.WriteLine(PrintSumOfIndexedNumber(myArr, 2));
+Console.WriteLine();
+
+
+
+
+ 
+
+Console.ReadKey();
